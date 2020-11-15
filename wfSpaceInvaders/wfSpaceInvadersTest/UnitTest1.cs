d82c849aaac771fcf71165caf6ee4ed603cbe82d
saads2018 <saadsultan2018@gmail.com>
2020-11-15 15:27:54 +0800

    add sample unit tests for checking

--- wfSpaceInvaders/wfSpaceInvadersTest/UnitTest1.cs
+++ wfSpaceInvaders/wfSpaceInvadersTest/UnitTest1.cs
@@ -5,18 +5,42 @@ namespace wfSpaceInvadersTest
     [TestFixture]
     public class Tests
     {
-       
 
+        int a;
+        int b;
         [SetUp]
         public void Setup()
         {
-            
+            a = 16;
+            b = 4;
         }
 
         [Test]
-        public void Test1()
+        public void CheckSum()
         {
-            
+            Assert.AreEqual(20, a + b);
         }
+
+        [Test]
+        public void CheckDifference()
+        {
+            Assert.AreEqual(12, a - b);
+        }
+
+        [Test]
+        public void CheckMultiplication()
+        {
+            Assert.AreEqual(64, a * b);
+        }
+
+
+        [Test]
+        public void CheckDivision()
+        {
+            Assert.AreEqual(4, a / b);
+        }
+
+
+
     }
 }
