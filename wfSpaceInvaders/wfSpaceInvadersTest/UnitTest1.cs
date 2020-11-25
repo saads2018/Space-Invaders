92df12fef4b4aec16f3e31d3d26ccb50d26fac43
saads2018 <71264405+saads2018@users.noreply.github.com>
2020-11-25 19:58:29 +0800

    Add files via upload

---
+++ wfSpaceInvaders/wfSpaceInvadersTest/UnitTest1.cs
@@ -0,0 +1,42 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace wfSpaceInvadersTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CheckSum()
+        {
+            int a =10; 
+            int b=24;
+            Assert.AreEqual(34, a + b);           
+        }
+
+        [TestMethod]
+        public void CheckDivision()
+        {
+            int a = 10;
+            int b = 30;
+            Assert.AreEqual(3, b/a);
+        }
+
+        [TestMethod]
+        public void CheckMultiplication()
+        {
+            int a = 10;
+            int b = 24;
+            Assert.AreEqual(240, a*b);
+        }
+
+
+        [TestMethod]
+        public void CheckDifference()
+        {
+            int a = 10;
+            int b = 24;
+            Assert.AreEqual(14, b-a);
+        }
+
+    }
+}
